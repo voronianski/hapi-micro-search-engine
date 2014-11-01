34df80277e30a12841804e13749544c19191deb0
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-11-01 14:59:53 +0200

    tdd

---
+++ test/app.spec.js
@@ -0,0 +1,21 @@
+require('should');
+
+var Lab = require('lab');
+var Server = require('../app');
+
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+
+describe('micro search engine', function () {
+    describe('when POSTing to /dictionary endpoint', function () {
+        describe('when GETting existing string with /search endpoint', function () {
+
+        });
+    });
+
+    describe('when GETting NON-existing string with /search endpoint', function () {
+
+    });
+});
